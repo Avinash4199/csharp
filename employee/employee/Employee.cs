f2e4633c4fe3fa732499352c0d025d137af6262f
Avinash4199 <191fa04199@gmail.com>
2025-01-29 15:52:06 +0530

    Add files via upload
    employee day3 wipro

---
+++ employee/employee/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassMethodStaticDemo
+{
+    internal class Employee
+    {
+        private int id;
+        private string? name;
+        private string? dpt;
+        private decimal sal;
+
+        public Employee(int id, string? name, string? dpt, decimal sal)
+        {
+            this.id = id;
+            this.name = name;
+            this.dpt = dpt;
+            this.sal = sal;
+        }
+
+        public string EmpId { get; internal set; }
+        public string EmpName { get; internal set; }
+    }
+}
